df95f7c396ec6dd62a06169b3785c2f1d8929d83
Kevin Yugla <kiyugla@espe.edu.ec>
2025-05-21 14:05:38 -0500

    creacion del codigo para tabal de multiplicar comentado

--- js/tablaMultiplicar.js
+++ js/tablaMultiplicar.js
@@ -1,1 +1,7 @@
+// Solicita al usuario un número para mostrar su tabla de multiplicar
 let numero = parseInt(prompt("Ingrese un número para mostrar su tabla de multiplicar:"));
+
+// Recorre del 1 al 12 mostrando cada resultado
+for (let i = 1; i <= 12; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+}
