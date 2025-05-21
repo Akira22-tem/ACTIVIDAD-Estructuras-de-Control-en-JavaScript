b82de9dd8a08cbd861e1db6d87f8cd8f174187cd
Kevin Yugla <kiyugla@espe.edu.ec>
2025-05-21 13:54:04 -0500

    creacion de la estructura if segun el rango de edad

--- js/edadCategoria.js
+++ js/edadCategoria.js
@@ -1,1 +1,15 @@
+// Solicita al usuario su edad y la convierte a número entero
 let edad = parseInt(prompt("Ingrese su edad:"));
+
+// Clasifica según el rango de edad
+if (edad < 0) {
+    console.log("Edad no válida");
+} else if (edad <= 12) {
+    console.log("Niño");
+} else if (edad <= 17) {
+    console.log("Adolescente");
+} else if (edad <= 59) {
+    console.log("Adulto");
+} else {
+    console.log("Adulto mayor");
+}
